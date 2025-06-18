27c30ffd4fef802076c21f6859afaf50018994ec
Youness elkinani <kinanix7>
2025-06-18 10:06:52 +0100

    Création de l’arborescence backend : controller, service, repository, model, dto, config

--- src/main/java/com/deliverymatch/DeliveryMatchApplication.java
+++ src/main/java/com/deliverymatch/DeliveryMatchApplication.java
@@ -1,13 +1,12 @@
-package com.example.deliverymatch;
+package com.deliverymatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeliveryMatchApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DeliveryMatchApplication.class, args);
     }
-
 }
+
